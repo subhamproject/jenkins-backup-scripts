0c8d3de15820e181a4ccb925796150c8259513a7
subhamproject <subhammandal01@gmail.com>
2018-11-21 13:18:59 +0530

    Update Jenkins-plugins-current-version.sh

--- Jenkins-plugins-current-version.sh
+++ Jenkins-plugins-current-version.sh
@@ -2,12 +2,12 @@
 JENKINS_HOME="${1:-/jenkins}"
 PLUGIN_DIR="${JENKINS_HOME}/plugins"
 DIR_LIST="$(ls -l $PLUGIN_DIR |grep ^d|awk '{print $NF}')"
-echo "**********Plugin version details***********" > /tmp/Plugins_version_list.txt
+echo "**********Plugin version details from `hostname`***********" > /tmp/Plugins_version_list.txt
 for DIR in $DIR_LIST
 do
 if [ -d $PLUGIN_DIR/$DIR ]
 then
 VERSION=$(find $PLUGIN_DIR/$DIR -name pom.properties |xargs cat |grep version|cut -d'=' -f2)
-echo "Plugin Version \"$DIR\" --> $VERSION" >> /tmp/Plugins_version_list.txt
+echo "$DIR:$VERSION" >> /tmp/Plugins_version_list.txt
 fi
 done
