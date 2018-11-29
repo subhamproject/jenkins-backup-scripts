393cbfd81ad23b2092f24b2ee51365c7f54d0188
subhamproject <subhammandal01@gmail.com>
2018-11-29 12:54:09 +0530

    Update jenkins-master-script.sh

--- jenkins-master-script.sh
+++ jenkins-master-script.sh
@@ -17,24 +17,24 @@ echo " "
 function current_plugins_version ()
 {
        DIR_LIST="$(ls -l $PLUGIN_DIR |grep ^d|awk '{print $NF}')"
-       echo "**********Plugin version details from `hostname`***********" > /tmp/Plugins_version_$$_list_`hostname`.txt
+       echo "**********Plugin version details from `hostname`***********" > ${JENKINS_HOME}/Current_plugins_version_$$_list_`hostname`.txt
        for DIR in $DIR_LIST
        do
        if [ -d $PLUGIN_DIR/$DIR ]
        then
        #VERSION=$(find $PLUGIN_DIR/$DIR -name pom.properties |xargs cat |grep version|cut -d'=' -f2)
        VERSION=$(find $PLUGIN_DIR/$DIR -name MANIFEST.MF|xargs cat|grep "Plugin-Version: "|cut -d':' -f2|tr -d ' ')
-       echo "$DIR:$VERSION" >> /tmp/Plugins_version_$$_list_`hostname`.txt
+       echo "$DIR:$VERSION" >> ${JENKINS_HOME}/Current_plugins_version_$$_list_`hostname`.txt
       fi
       done
-      echo "Current plugin version details are taken..output can be found in /tmp/Plugins_version_$$_list_`hostname`.txt"
+      echo "Current plugin version details are taken..output can be found in ${JENKINS_HOME}/Current_plugins_version_$$_list_`hostname`.txt"
 }
 
 ### Function to take backup of plugins which are to be upgraded in server #######
 function backup_plugins ()
 {
      [ ! -d $BACKUP_DIR ] && mkdir -p $BACKUP_DIR
-     for PLUGINS_LIST in $(cat /tmp/file.txt) ## /tmp/file.txt is a file where plugins details need to be put in which are to be updated
+     for PLUGINS_LIST in $(cat ${JENKINS_HOME}/Plugins_to_be_updated.txt) ## ${JENKINS_HOME}/Plugins_to_be_updated.txt is a file where plugins details need to be put in which are to be updated
      do
      [ -d $PLUGIN_DIR/$(echo $PLUGINS_LIST|cut -d':' -f1) ] && mv $PLUGIN_DIR/$(echo $PLUGINS_LIST|cut -d':' -f1) $BACKUP_DIR/
      [ -f $PLUGIN_DIR/$(echo $PLUGINS_LIST|cut -d':' -f1).jpi ] && mv $PLUGIN_DIR/$(echo $PLUGINS_LIST|cut -d':' -f1).jpi* $BACKUP_DIR/
@@ -61,7 +61,7 @@ function upgrade_plugins () {
    fi
    }
 
-  for plugin in $(cat /tmp/file.txt) ## /tmp/file.txt is a file where plugins details need to be put in which are to be updated
+  for plugin in $(cat ${JENKINS_HOME}/Plugins_to_be_updated.txt) ## ${JENKINS_HOME}/Plugins_to_be_updated.txt is a file where plugins details need to be put in which are to be updated
   do
   Plugin_install "$plugin"
   done
@@ -71,8 +71,23 @@ function upgrade_plugins () {
   echo "Installing dependencies for $f"
   deps=$( unzip -p ${f} META-INF/MANIFEST.MF | tr -d '\r' | sed -e ':a;N;$!ba;s/\n //g' | grep -e "^Plugin-Dependencies: " | awk '{ print $2 }' | tr ',' '\n' |cut -d';' -f1 )
   for plugin in $deps; do
-  Plugin_install "$plugin"
+  #Plugin_install "$plugin"
+  echo "${plugin}" >> /tmp/depency_$$_file.txt
   done
+  done
+  cat /tmp/depency_$$_file.txt|sort -V >> /tmp/deps_$$_file.txt
+  declare -A array
+  while IFS=':' read key value
+  do
+  array[$key]=$value
+  done < /tmp/deps_$$_file.txt
+  for key in ${!array[@]}
+  do
+  echo $key:${array[$key]}
+  done >> /tmp/deps_$$_higher_version.txt
+  for plugins in $(cat /tmp/deps_$$_higher_version.txt|sort -V)
+  do
+  Plugin_install "$plugins"
   done
   echo "fixing permissions"
   chown -R ${file_owner} ${PLUGIN_DIR}
